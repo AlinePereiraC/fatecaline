1234390ec82001cf19434893181fb53cc31ab398
AlinePereiraC <126829018+AlinePereiraC@users.noreply.github.com>
2023-03-02 20:25:25 -0300

    inicio atividade

---
+++ JS/jscript.js
@@ -0,0 +1,7 @@
+const a = Number(prompt("Digite o valor do seu salario: "))
+const b = Number(prompt("Digite a porcentagem do aumento: "))
+
+resultado = a*((b/100)+1)
+
+//console.log(resultado)
+document.write(resultado)
